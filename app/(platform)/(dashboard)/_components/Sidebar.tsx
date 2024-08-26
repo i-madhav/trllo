abe0389db403be9ac05330c9c2729f03ab8c3950
Madhav <madhav.shar06ma@gmail.com>
2024-08-26 22:33:09 +0530

    further coding

--- app/(platform)/(dashboard)/_components/Sidebar.tsx
+++ app/(platform)/(dashboard)/_components/Sidebar.tsx
@@ -1,13 +1,54 @@
-"use client"
+"use client";
+import Link from "next/link";
+import { Plus } from "lucide-react";
+import { useLocalStorage } from "usehooks-ts";
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Accordion } from "@/components/ui/accordion";
+
 interface SidebarProps {
-    
+  // used to keep track of what was open and what was not open in the side bar!!
+  storageKey?: string;
 }
 
-const Sidebar = () => {
+const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
+  const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
+    storageKey,
+    {}
+  );
+
+  const { 
+    organization: activeOrganization,
+    isLoaded:isLoadedOrg
+   } = useOrganization();
+
+
+  const{
+    userMemberships,
+    isLoaded:isLoadedOrgList
+  } = useOrganizationList({
+    userMemberships:{
+      infinite:true,
+    }
+  });
+
+  const defaultAccordianValue:string[] = Object.keys(expanded).reduce((acc:string[] , key:string) => {
+    if(expanded[key]){
+      acc.push(key);
+    }
+    return acc;
+  },[]);
+
+  const onExpand = (id:string) => {
+    
+  }
 
   return (
-    <div>Sidebar</div>
-  )
-}
+  <div>Sidebar</div>
 
-export default Sidebar
+  );
+};
+
+export default Sidebar;
--- app/(platform)/(dashboard)/organization/[organizatiionId]/_components/org-control.tsx
+++ app/(platform)/(dashboard)/organization/[organizatiionId]/_components/org-control.tsx
@@ -1,22 +1,22 @@
-"use client"
-import React from 'react'
-import { useEffect } from 'react'
-import { useParams } from 'next/navigation';
-import { useOrganizationList } from '@clerk/nextjs';
+"use client";
+import React from "react";
+import { useEffect } from "react";
+import { useParams } from "next/navigation";
+import { useOrganizationList } from "@clerk/nextjs";
 
+// each organisation have a particular id now what if i change the id manually in the url , earlier it was not changing but now due to orgcontrol it changes as per it's org id !!
 const OrgControl = () => {
-    const params = useParams();
-    const {setActive} = useOrganizationList(); 
+  const params = useParams();
+  const { setActive } = useOrganizationList();
 
-    useEffect(() => {
-        if(!setActive) return;
+  useEffect(() => {
+    if (!setActive) return;
 
-        setActive({
-            organization:params.organizatiionId as string,
-        });
-
-    },[setActive,params.organizationId])
+    setActive({
+      organization: params.organizatiionId as string,
+    });
+  }, [setActive, params.organizationId]);
   return null;
-}
+};
 
 export default OrgControl;
